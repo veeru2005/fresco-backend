e4834a577efb694461dfdfe38e26c8234ca2af81
veeru2005 <sunkavalli.veerendra1973@gmail.com>
2026-04-03 15:34:40 +0530

    backend

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,8 @@ module.exports = {
       instances: 1,
       exec_mode: "fork",
       env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
         NODE_ENV: "production",
-        // MONGO_URI, JWT_SECRET, PORT will be loaded from the VM's environment or .env file
+        PORT: 3000
       }
     }
   ]
