250967855d5e85affa016306acfed213809d2d73
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2026-04-09 00:22:13 +0530

    backend

--- server.js
+++ server.js
@@ -147,3 +147,4 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/fresco_orga
         console.error('Error connecting to MongoDB:', error);
         process.exit(1);
     });
+
